5bc7f0c1d29a54c321dd84c8bb96f06c8636697f
lrjin <659618619@qq.com>
2019-01-17 13:19:35 +0800

    Signed-off-by: lrjin <659618619@qq.com>

--- eureka-server/src/main/java/com/eureka/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/eureka/eurekaserver/EurekaServerApplication.java
@@ -2,6 +2,7 @@ package com.eureka.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
@@ -9,6 +10,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication app = new SpringApplication(EurekaServerApplication.class);
+		app.addListeners(new ApplicationPidFileWriter());
+		app.run(args);
+		//SpringApplication.run(EurekaServerApplication.class, args);
 	}
 }
